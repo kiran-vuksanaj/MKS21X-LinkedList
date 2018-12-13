6ee3851d9c864dd789dcc4d218425fbec30ba887
kiran-vuksanaj <mvuksanaj00@stuy.edu>
2018-12-12 19:55:58 -0500

    fields, add, toString hypothetical

---
+++ MyLinkedList.java
@@ -0,0 +1,31 @@
+public class MyLinkedList{
+  private int size;
+  private Node start,end;
+  public MyLinkedList(){
+    size=0;
+  }
+  public int size(){
+    return size;
+  }
+  public boolean add(int value){
+    if(size()==0){
+      start = new Node(value,null,null);
+      end = start;
+      size++;
+    }else{
+      end.addAfter(value);
+      end = end.next();
+      size++;
+    }
+    return true;
+  }
+  public String toString(){
+    String out = "[";
+    Node current = start;
+    while(current != null){
+      out+=current.get()+",";
+      current = current.next();
+    }
+    return out+"]";
+  }
+}
