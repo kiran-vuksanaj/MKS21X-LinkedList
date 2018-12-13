d534922850db41179110e129193469049a5351d2
kiran-vuksanaj <mvuksanaj00@stuy.edu>
2018-12-12 20:00:04 -0500

    brief driver file

---
+++ Driver.java
@@ -0,0 +1,10 @@
+public class Driver{
+  public static void main(String[] args){
+    MyLinkedList MLL = new MyLinkedList();
+    MLL.add(4);
+    MLL.add(7);
+    MLL.add(19);
+    MLL.add(-3);
+    System.out.println(MLL);
+  }
+}
