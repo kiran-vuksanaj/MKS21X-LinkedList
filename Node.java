834fa8e528b9a9530dce6cc3533bae95148ec23d
kiran-vuksanaj <mvuksanaj00@stuy.edu>
2018-12-12 19:55:34 -0500

    hypothetical accessor + basic change methods

---
+++ Node.java
@@ -0,0 +1,40 @@
+public class Node{
+  private int data;
+  private Node next,prev;
+  public Node(int data,Node next,Node prev){
+    this.data = data;
+    this.next = next;
+    this.prev = prev;
+  }
+  public int get(){
+    return data;
+  }
+  public boolean set(int data){
+    this.data = data;
+    return true;
+  }
+  public Node prev(){
+    return prev;
+  }
+  public Node next(){
+    return next;
+  }
+  private boolean setPrev(Node n){
+    prev = n;
+    return true;
+  }
+  private boolean setNext(Node n){
+    next = n;
+    return true;
+  }
+  public boolean addAfter(int val){
+    next = new Node(val,next,this);
+    next.next().setPrev(next);
+    return true;
+  }
+  public boolean remove(){
+    next.setPrev(prev);
+    prev.setNext(next);
+    return true;
+  }
+}
